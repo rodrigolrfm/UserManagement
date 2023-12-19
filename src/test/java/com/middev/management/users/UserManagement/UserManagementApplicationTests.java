c85f17cb1520fcf8063a522fd4f614c888d3133a
rodrigolrfm <31398550+rodrigolrfm@users.noreply.github.com>
2023-12-19 01:07:28 -0500

    first commit

---
+++ src/test/java/com/middev/management/users/UserManagement/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.middev.management.users.UserManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
